4bcc02854224045fae863a5a22d8374d3b718386
sreeman <sreemancodex@gmail.com>
2026-02-04 20:04:39 +0530

    uploaded the list_duplicates

---
+++ list_duplicates.py
@@ -0,0 +1,12 @@
+def list(list_1):
+    count=0
+    for i in range(0,len(list_1)-1):
+        if(list_1[i]==5):
+            if(list_1[i]==5):
+                count+=1
+    
+            else:
+                print("The number is not repeated") 
+    print("Duplicates:",count)            
+            
+list([2,3,5,6,5,5,5,7,5,9,5])                          
